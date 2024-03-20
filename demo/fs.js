61ac4c87d0b479855349b9d3fde7be2655bb611d
muskus5 <oaokuznetcov@mail.ru>
2024-03-20 15:11:57 +0300

    change in file fs.js

--- demo/fs.js
+++ demo/fs.js
@@ -2,4 +2,28 @@
 const fs = require("fs");
 const path = require("path");
 
-fs.mkdir;
+// fs.mkdir(path.join(__dirname, "test"), (err) => {
+//   if (err) {
+//     throw err;
+//   }
+
+//   console.log("Папка создана");
+// });
+
+const filePath = path.join(__dirname, "test", "text.txt");
+
+fs.writeFile(filePath, "Hello NodeJS!", (err) => {
+  if (err) {
+    throw err;
+  }
+
+  console.log("Файл создан");
+
+  fs.appendFile(filePath, "\nHello Again!", (err) => {
+    if (err) {
+      throw err;
+    }
+
+    console.log("Файл создан");
+  });
+});

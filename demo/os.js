d54857798baae9e05aeef3e4777cccac63e8d44e
muskus5 <oaokuznetcov@mail.ru>
2024-03-21 19:32:07 +0300

    change in file os.js

--- demo/os.js
+++ demo/os.js
@@ -4,4 +4,6 @@ console.log("Операционная система:", os.platform());
 
 console.log("Архитектура процессора:", os.arch());
 
-console.log("Инфа по процессорам:");
+console.log("Инфа по процессорам: ", os.cpus());
+
+console.log("Свободная память: ", os.freemem());

d3e5eec1b919bc1589cb624586447697050eb817
muskus5 <oaokuznetcov@mail.ru>
2024-03-23 16:44:21 +0300

    add setTimeout() in events.js

--- demo/events.js
+++ demo/events.js
@@ -7,3 +7,8 @@ emitter.on("anything", (data) => {
 });
 
 emitter.emit("anything", { a: 1 });
+emitter.emit("anything", { b: 2 });
+
+setTimeout(() => {
+  emitter.emit("anything", { c: 3 });
+}, 500);

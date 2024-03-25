e323f49edb5c7a3db1dbe22757a08f22f72b095d
muskus5 <oaokuznetcov@mail.ru>
2024-03-25 17:07:28 +0300

    change two in file index.js

--- index.js
+++ index.js
@@ -6,3 +6,11 @@
 // console.log(__filename);
 
 const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.end(<h1>Hello NodeJS</h1>);
+});
+
+server.listen(3000, () => {
+  console.log("Server has been started...");
+});

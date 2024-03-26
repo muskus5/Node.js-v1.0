94162d2485273de5b16423cc7a2b0218f2b6517e
muskus5 <oaokuznetcov@mail.ru>
2024-03-26 17:19:12 +0300

    add file index.html

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-// const chalk = require("chalk");
-// const text = require("./data");
-
-// console.log(chalk.blue(text));
-
-// console.log(__filename);
-
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end('<h1>Hello NodeJS</h1>');
+  res.writeHead(200, {
+    "Content-Type": "text/html",
+  });
+  res.end("<h1>Hello NodeJS!!!</h1>");
 });
 
 server.listen(3000, () => {

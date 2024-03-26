fc817e39abdc0c298f66f94f79283e5e3567ecd7
muskus5 <oaokuznetcov@mail.ru>
2024-03-26 17:05:11 +0300

    change in file package.json

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end(<h1>Hello NodeJS</h1>);
+  res.end('<h1>Hello NodeJS</h1>');
 });
 
 server.listen(3000, () => {

148dcb2f51209d9aa24209d3a15ee80727476931
muskus5 <oaokuznetcov@mail.ru>
2024-03-27 17:56:56 +0300

    change two in file index.js

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
     "Content-Type": "text/html",
   });
+  if (req.url === "/") {
+    fs.readFile(path.join(__dirname, "publick", "index.html"), (err, data) => {
+      if (err) {
+        throw err
+      }
+    });
+  }
   res.end("<h1>Hello NodeJS!!!</h1>");
 });
 

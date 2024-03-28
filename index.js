c21c7e0b7e87b6cc5d766349499d91a13f09e5da
muskus5 <oaokuznetcov@mail.ru>
2024-03-28 13:53:19 +0300

    change three in file index.js

--- index.js
+++ index.js
@@ -3,17 +3,37 @@ const fs = require("fs");
 const path = require("path");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    "Content-Type": "text/html",
+  // if (req.url === "/") {
+  //   fs.readFile(path.join(__dirname, "public", "index.html"), (err, data) => {
+  //     if (err) {
+  //       throw err;
+  //     }
+  //     res.writeHead(200, {
+  //       "Content-Type": "text/html",
+  //     });
+  //     res.end(data);
+  //   });
+  // } else if (req.url === "/contact") {
+  //   fs.readFile(path.join(__dirname, "public", "contact.html"), (err, data) => {
+  //     if (err) {
+  //       throw err;
+  //     }
+  //     res.writeHead(200, {
+  //       "Content-Type": "text/html",
+  //     });
+  //     res.end(data);
+  //   });
+  // }
+  let filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "index.html" : req.url
+  );
+  console.log(filePath);
+  fs.readFile(filePath, () => {
+    
   });
-  if (req.url === "/") {
-    fs.readFile(path.join(__dirname, "publick", "index.html"), (err, data) => {
-      if (err) {
-        throw err
-      }
-    });
-  }
-  res.end("<h1>Hello NodeJS!!!</h1>");
+  res.end();
 });
 
 server.listen(3000, () => {
